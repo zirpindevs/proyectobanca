a7504b9816df57df7b132ccea11c633247a203b5
zirpin <zirpin@gmail.com>
2021-05-12 12:47:57 +0200

    categorias

--- src/main/java/com/example/proyectobanca/model/Categorias.java
+++ src/main/java/com/example/proyectobanca/model/Categorias.java
@@ -1,7 +1,35 @@
 package com.example.proyectobanca.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Categorias {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name="nombre")
+    private String nombre;
+
+    public Categorias() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Categorias{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
 }
