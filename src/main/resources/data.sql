6909108c49f23b6b773a7c6679995ed94ba32f24
zirpin <zirpin@gmail.com>
2021-05-14 12:01:12 +0200

    ejemplos data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,20 @@ insert into categories(name) values ('Gasolina');
 insert into categories(name) values ('Restauración');
 insert into categories(name) values ('Alimentación');
 insert into categories(name) values ('Otros');
+
+-- Bank Account examples
+insert into bank_accounts(id, numero_cuenta, saldo, created_date) values (1, '02601732345271851', '500', "2020-02-11 01:00:00");
+insert into bank_accounts(id, numero_cuenta, saldo, created_date) values (2, '07941320452229156', '1500', "2019-02-11 01:00:00");
+
+
+-- Credit Card examples
+
+insert into credit_cards(id, num_credit_card, placeholder, type, card_provider, cvv, pin, id_user, expiration_date) values (1, '5254646244652394', 'Thomas Conley', 'debit', 'MasterCard', '239', '1234', 1, "2041-02-11 01:00:00");
+insert into credit_cards(id, num_credit_card, placeholder, type, card_provider, cvv, pin, id_user, expiration_date) values (2, '8254645534652344', 'John Connor', 'debit', 'Visa', '122', '1234', 2, "2031-07-11 01:00:00");
+
+-- Transaction examples
+insert into transactions(id, importe, concepto, tipo_movimiento, id_bank_account, created_date) values (1, '350', "pago coche", 'ingreso', 1, "2021-02-11 01:00:00");
+insert into transactions(id, importe, concepto, tipo_movimiento, id_bank_account, created_date) values (3, '50', 'alquiler garaje', "pago", 1, "2020-02-11 01:00:00");
+insert into transactions(id, importe, concepto, tipo_movimiento, id_bank_account, created_date) values (2, '500', "nomina", 'domiciliacion', 2, "2021-04-11 01:00:00");
+
+
