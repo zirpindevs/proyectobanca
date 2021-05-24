4ed0a692a3bbb5f1dc0e5dd9d5d9a09edbd1a914
zirpin <zirpin@gmail.com>
2021-05-24 15:42:29 +0200

    add merge changes

--- src/main/java/com/example/proyectobanca/controller/TransactionOperationsController.java
+++ src/main/java/com/example/proyectobanca/controller/TransactionOperationsController.java
@@ -4,6 +4,7 @@ import com.example.proyectobanca.model.Transaction;
 import com.example.proyectobanca.model.transaction.operations.DailyBalanceRange;
 import com.example.proyectobanca.model.transaction.operations.DailyBalanceResponse;
 import com.example.proyectobanca.model.transaction.operations.UserDailyBalanceResponse;
+import com.example.proyectobanca.model.transaction.operations.totalOperations.DailyOperationsResponse;
 import com.example.proyectobanca.model.transaction.operations.totalTransactions.DailyTransactionResponse;
 import com.example.proyectobanca.service.TransactionOperationsService;
 import io.swagger.annotations.ApiOperation;
@@ -37,11 +38,12 @@ public class TransactionOperationsController {
     /**
      * Controller:
      * Get the Balance and Total of Transactions per day and the total transactions between two dates for a bank account
-     * @param id Id of bank account that you have get the balance
+     *
+     * @param id        Id of bank account that you have get the balance
      * @param startDate Start date to obtain the daily balance
-     * @param endDate End date to obtain the daily balance
-     * @param page Page to be displayed of the results obtained (optional)
-     * @param limit Number of records per page that you want to show of the results obtained (optional)
+     * @param endDate   End date to obtain the daily balance
+     * @param page      Page to be displayed of the results obtained (optional)
+     * @param limit     Number of records per page that you want to show of the results obtained (optional)
      * @return DailyBalanceResponse with List of the Balance and Total of Transactions per day between two dates from database
      */
     @GetMapping("/transactions-operations/daily-balance-bankaccount/{id}")
@@ -80,11 +82,12 @@ public class TransactionOperationsController {
     /**
      * Controller:
      * Get the Balance and Total of Transactions per day and the total transactions between two dates for a bank account
-     * @param userId The user ID that you want to get the daily balance of all your accounts
+     *
+     * @param userId    The user ID that you want to get the daily balance of all your accounts
      * @param startDate Start date to obtain the daily balance
-     * @param endDate End date to obtain the daily balance
-     * @param page Page to be displayed of the results obtained (optional)
-     * @param limit Number of records per page that you want to show of the results obtained (optional)
+     * @param endDate   End date to obtain the daily balance
+     * @param page      Page to be displayed of the results obtained (optional)
+     * @param limit     Number of records per page that you want to show of the results obtained (optional)
      * @return UserDailyBalanceResponse with List of the Balance and Total of Transactions per day between two dates of all user accounts
      */
     @GetMapping("/transactions-operations/daily-balance-user/{userId}")
@@ -121,190 +124,18 @@ public class TransactionOperationsController {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     //************************************************************************************************************/
-
 
 
     /**
      * Controller:
      * Get the number of trasactions per day between two dates for a creditcard
-     * @param id Id of creditcard that you have get the balance
+     *
+     * @param id        Id of creditcard that you have get the balance
      * @param startDate Start date to obtain the daily balance
-     * @param endDate End date to obtain the daily balance
-     * @param page Page to be displayed of the results obtained (optional)
-     * @param limit Number of records per page that you want to show of the results obtained (optional)
+     * @param endDate   End date to obtain the daily balance
+     * @param page      Page to be displayed of the results obtained (optional)
+     * @param limit     Number of records per page that you want to show of the results obtained (optional)
      * @return DailyBalanceResponse with List of the balance per day of transactions between two dates from database
      */
     @GetMapping("/transactions-operations/daily-transaction-creditcard/{id}")
@@ -327,6 +158,95 @@ public class TransactionOperationsController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
         DailyTransactionResponse result = transactionOperationsService.getDailyTransactionByDateRangeByCreditCard(id, map1);
+
+        if (result.getStatus() == "-404")
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        if (result.getStatus() == "-500")
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        if (result.getStatus() == "-204")
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        return ResponseEntity.ok().body(result);
+    }
+
+
+    /**
+     * Controller:
+     * Get the number of operations per day between two dates for a bankaccount
+     *
+     * @param id        Id of bankaccout that you have get the operations
+     * @param startDate Start date to obtain the daily operations
+     * @param endDate   End date to obtain the daily operations
+     * @param page      Page to be displayed of the results obtained (optional)
+     * @param limit     Number of records per page that you want to show of the results obtained (optional)
+     * @return DailyOperationsResponse with List of the operations per day of a bankaccount between two dates from database
+     */
+    @GetMapping("/transactions-operations/daily-operations-category-bankaccount/{id}")
+    @ApiOperation("Get number of operations per day between two dates")
+    public ResponseEntity<DailyOperationsResponse> getAllOperationsByCategoryBankAccount(
+            @ApiParam("Primary key of bank account: Long") @PathVariable Long id,
+            @ApiParam("Start date: LocalDate") @QueryParam("startDate") String startDate,
+            @ApiParam("End date: LocalDate") @QueryParam("endDate") String endDate,
+            @ApiParam("Pagination: page from which the records start to be displayed (optional): Integer") @QueryParam("page") String page,
+            @ApiParam("Pagination: number of records displayed per page (optional): Integer") @QueryParam("limit") String limit
+    ) {
+
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("startDate", startDate + " 00:00:00.000000");
+        map1.put("endDate", endDate + " 23:59:59.999999");
+        map1.put("page", page);
+        map1.put("limit", limit);
+
+        if (startDate == null || endDate == null)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        DailyOperationsResponse result = transactionOperationsService.getAllOperationsByCategoryBankAccount(id, map1);
+
+        if (result.getStatus() == "-404")
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        if (result.getStatus() == "-500")
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        if (result.getStatus() == "-204")
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        return ResponseEntity.ok().body(result);
+    }
+
+    /**
+     * Controller:
+     * Get the number of operations per day between two dates for a creditcard
+     *
+     * @param id        Id of creditcard that you have get the operations
+     * @param startDate Start date to obtain the daily operations
+     * @param endDate   End date to obtain the daily operations
+     * @param page      Page to be displayed of the results obtained (optional)
+     * @param limit     Number of records per page that you want to show of the results obtained (optional)
+     * @return DailyOperationsResponse with List of the operations per day of a creditcard between two dates from database
+     */
+    @GetMapping("/transactions-operations/daily-operations-category-creditcard/{id}")
+    @ApiOperation("Get number of operations per day between two dates")
+    public ResponseEntity<DailyOperationsResponse> getAllOperationsByCategoryCreditCard(
+            @ApiParam("Primary key of credit card: Long") @PathVariable Long id,
+            @ApiParam("Start date: LocalDate") @QueryParam("startDate") String startDate,
+            @ApiParam("End date: LocalDate") @QueryParam("endDate") String endDate,
+            @ApiParam("Pagination: page from which the records start to be displayed (optional): Integer") @QueryParam("page") String page,
+            @ApiParam("Pagination: number of records displayed per page (optional): Integer") @QueryParam("limit") String limit
+    ) {
+
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("startDate", startDate + " 00:00:00.000000");
+        map1.put("endDate", endDate + " 23:59:59.999999");
+        map1.put("page", page);
+        map1.put("limit", limit);
+
+        if (startDate == null || endDate == null)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        DailyOperationsResponse result = transactionOperationsService.getAllOperationsByCategoryCreditCard(id, map1);
 
         if (result.getStatus() == "-404")
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
--- src/main/java/com/example/proyectobanca/service/TransactionOperationsService.java
+++ src/main/java/com/example/proyectobanca/service/TransactionOperationsService.java
@@ -3,6 +3,7 @@ package com.example.proyectobanca.service;
 import com.example.proyectobanca.model.transaction.operations.DailyBalanceRange;
 import com.example.proyectobanca.model.transaction.operations.DailyBalanceResponse;
 import com.example.proyectobanca.model.transaction.operations.UserDailyBalanceResponse;
+import com.example.proyectobanca.model.transaction.operations.totalOperations.DailyOperationsResponse;
 import com.example.proyectobanca.model.transaction.operations.totalTransactions.DailyTransactionResponse;
 
 import java.util.List;
@@ -15,5 +16,8 @@ public interface TransactionOperationsService {
     UserDailyBalanceResponse getDailyBalanceByDateRangeByUser(Long idUser, Map<String, String> map1);
 
     DailyTransactionResponse getDailyTransactionByDateRangeByCreditCard(Long idCreditCard, Map<String, String> map1);
+
+    DailyOperationsResponse getAllOperationsByCategoryBankAccount(Long idBankAccount, Map<String, String> map1);
+    DailyOperationsResponse getAllOperationsByCategoryCreditCard(Long idCreditCard, Map<String, String> map1);
 
     }
