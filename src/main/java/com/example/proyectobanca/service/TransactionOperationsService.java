fab59a17c273a520028ab215b565d563903c7baf
scarrasco85 <chechucarrasco@gmail.com>
2021-05-24 13:45:10 +0200

    Balances getDailyBalanceByDateRangeByUser ready rollback

--- src/main/java/com/example/proyectobanca/service/TransactionOperationsService.java
+++ src/main/java/com/example/proyectobanca/service/TransactionOperationsService.java
@@ -2,6 +2,7 @@ package com.example.proyectobanca.service;
 
 import com.example.proyectobanca.model.transaction.operations.DailyBalanceRange;
 import com.example.proyectobanca.model.transaction.operations.DailyBalanceResponse;
+import com.example.proyectobanca.model.transaction.operations.UserDailyBalanceResponse;
 import com.example.proyectobanca.model.transaction.operations.totalTransactions.DailyTransactionResponse;
 
 import java.util.List;
@@ -14,8 +15,5 @@ public interface TransactionOperationsService {
     UserDailyBalanceResponse getDailyBalanceByDateRangeByUser(Long idUser, Map<String, String> map1);
 
     DailyTransactionResponse getDailyTransactionByDateRangeByCreditCard(Long idCreditCard, Map<String, String> map1);
-    DailyOperationsResponse getAllOperationsByCategoryBankAccount(Long idBankAccount, Map<String, String> map1);
-    DailyOperationsResponse getAllOperationsByCategoryCreditCard(Long idCreditCard, Map<String, String> map1);
-
 
     }
